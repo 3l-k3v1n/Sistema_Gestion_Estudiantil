201ed3edf02e37864c6c8c333c7f3ee71a8aa1ca
David Bone <davidbone2004@gmail.com>
2026-06-28 13:55:21 -0500

    Creacion de la configuracion de sistema en singleton

--- src/singleton/ConfiguracionSistema.java
+++ src/singleton/ConfiguracionSistema.java
@@ -1,4 +1,29 @@
 package singleton;
 
 public class ConfiguracionSistema {
+
+    private static ConfiguracionSistema instancia;
+
+    private String nombreUniversidad;
+    private String ciclo;
+
+    private ConfiguracionSistema() {
+        this.nombreUniversidad = "Universidad Central";
+        this.ciclo = "2026-A";
+    }
+
+    public static ConfiguracionSistema getInstancia() {
+        if (instancia == null) {
+            instancia = new ConfiguracionSistema();
+        }
+        return instancia;
+    }
+
+    public String getNombreUniversidad() { return nombreUniversidad; }
+    public String getCiclo()             { return ciclo; }
+
+    @Override
+    public String toString() {
+        return "[" + nombreUniversidad + " | Ciclo: " + ciclo + "]";
+    }
 }
